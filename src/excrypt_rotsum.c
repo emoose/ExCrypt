3663aafe64ea5a793a9b3887b1dc9eff98799e97
emoose <abc@cock.li>
2024-10-05 12:46:06 +0100

    rotsum: move ExCryptRotSumSha to rotsum.c

--- src/excrypt_rotsum.c
+++ src/excrypt_rotsum.c
@@ -54,3 +54,32 @@ void ExCryptRotSum4(EXCRYPT_ROTSUM4_STATE* result, uint32_t* input, uint32_t inp
   result->data[0] = _byteswap_uint64(result->data[0]);
   result->data[1] = _byteswap_uint64(result->data[1]);
 }
+
+void ExCryptRotSumSha(const uint8_t* input1, uint32_t input1_size, const uint8_t* input2, uint32_t input2_size,
+  uint8_t* output, uint32_t output_size)
+{
+  EXCRYPT_ROTSUM_STATE rotsum;
+  memset(&rotsum, 0, sizeof(EXCRYPT_ROTSUM_STATE));
+
+  ExCryptRotSum(&rotsum, (const uint64_t*)input1, input1_size / 8);
+  ExCryptRotSum(&rotsum, (const uint64_t*)input2, input2_size / 8);
+
+  EXCRYPT_SHA_STATE sha;
+  ExCryptShaInit(&sha);
+
+  ExCryptShaUpdate(&sha, (const uint8_t*)&rotsum, sizeof(EXCRYPT_ROTSUM_STATE));
+  ExCryptShaUpdate(&sha, (const uint8_t*)&rotsum, sizeof(EXCRYPT_ROTSUM_STATE));
+
+  ExCryptShaUpdate(&sha, input1, input1_size);
+  ExCryptShaUpdate(&sha, input2, input2_size);
+
+  rotsum.data[0] = ~rotsum.data[0];
+  rotsum.data[1] = ~rotsum.data[1];
+  rotsum.data[2] = ~rotsum.data[2];
+  rotsum.data[3] = ~rotsum.data[3];
+
+  ExCryptShaUpdate(&sha, (const uint8_t*)&rotsum, sizeof(EXCRYPT_ROTSUM_STATE));
+  ExCryptShaUpdate(&sha, (const uint8_t*)&rotsum, sizeof(EXCRYPT_ROTSUM_STATE));
+
+  ExCryptShaFinal(&sha, output, output_size);
+}
