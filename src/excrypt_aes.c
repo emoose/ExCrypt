b9087d4d4d86c1e27a5d3a2ae92eed44cb5b23e2
emoose <abc@cock.li>
2024-10-28 03:21:15 +0000

    ExCryptAesCbc: fix IV issue when output == input
    Though I'm sure there's a better way than adding 2 new memcpys...

--- src/excrypt_aes.c
+++ src/excrypt_aes.c
@@ -159,16 +159,22 @@ void rijndaelCbcEncrypt(const unsigned long* rk, const uint8_t* input, uint32_t 
 
 void rijndaelCbcDecrypt(const unsigned long* rk, const uint8_t* input, uint32_t input_size, uint8_t* output, uint8_t* feed)
 {
-  const uint8_t* iv = feed;
+  uint8_t current_input[AES_BLOCKLEN];
+  uint8_t iv[AES_BLOCKLEN];
+  memcpy(iv, feed, AES_BLOCKLEN);
+
   for (uint32_t i = 0; i < input_size; i += AES_BLOCKLEN)
   {
+    memcpy(current_input, input, AES_BLOCKLEN);
     AesDec(rk, 10, input, output);
     xorWithIv(output, output, iv);
-    iv = input;
+    memcpy(iv, current_input, AES_BLOCKLEN);
+
     output += AES_BLOCKLEN;
     input += AES_BLOCKLEN;
   }
-  // store IV in feed param for next call
+
+  // Store the last ciphertext block in feed for the next call
   memcpy(feed, iv, AES_BLOCKLEN);
 }
 
@@ -220,16 +226,22 @@ void ExCryptAesCbcEncrypt(EXCRYPT_AES_SCHEDULE* state, const uint8_t* input, uin
 
 void ExCryptAesCbcDecrypt(EXCRYPT_AES_SCHEDULE* state, const uint8_t* input, uint32_t input_size, uint8_t* output, uint8_t* feed)
 {
-  const uint8_t* iv = feed;
+  uint8_t current_input[AES_BLOCKLEN];
+  uint8_t iv[AES_BLOCKLEN];
+  memcpy(iv, feed, AES_BLOCKLEN);
+
   for (uint32_t i = 0; i < input_size; i += AES_BLOCKLEN)
   {
+    memcpy(current_input, input, AES_BLOCKLEN);
     rijndaelDecrypt(aesschedule_dectable(state), state->num_rounds + 1, input, output);
     xorWithIv(output, output, iv);
-    iv = input;
+    memcpy(iv, current_input, AES_BLOCKLEN);
+
     output += AES_BLOCKLEN;
     input += AES_BLOCKLEN;
   }
-  // store IV in feed param for next call
+
+  // Store the last ciphertext block in feed for the next call
   memcpy(feed, iv, AES_BLOCKLEN);
 }
 
